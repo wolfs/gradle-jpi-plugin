19d4520cce2e10e6e6f9169dee2f9e26ba91f502
Daniel Spilker <mail@daniel-spilker.com>
2014-10-07 21:44:46 +0200

    fixed CodeNarc violations

--- src/main/groovy/org/jenkinsci/gradle/plugins/jpi/JpiManifest.groovy
+++ src/main/groovy/org/jenkinsci/gradle/plugins/jpi/JpiManifest.groovy
@@ -13,59 +13,67 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jenkinsci.gradle.plugins.jpi
 
 import java.text.SimpleDateFormat
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginConvention
-import org.gradle.api.tasks.SourceSet
 import org.gradle.api.artifacts.ResolvedArtifact
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.plugins.WarPluginConvention
+
+import java.util.jar.Manifest
+
+import static org.gradle.api.tasks.SourceSet.MAIN_SOURCE_SET_NAME
+import static org.jenkinsci.gradle.plugins.jpi.JpiPlugin.OPTIONAL_PLUGINS_DEPENDENCY_CONFIGURATION_NAME
+import static org.jenkinsci.gradle.plugins.jpi.JpiPlugin.PLUGINS_DEPENDENCY_CONFIGURATION_NAME
 
 /**
  * Encapsulates the Jenkins plugin manifest and its generation.
  *
  * @author Kohsuke Kawaguchi
  */
-class JpiManifest extends HashMap<String,Object> {
-    private final Project project;
+class JpiManifest extends HashMap<String, Object> {
+    private final Project project
 
     JpiManifest(Project project) {
         this.project = project
 
         def conv = project.extensions.getByType(JpiExtension)
-        def classDir = project.convention.getPlugin(JavaPluginConvention).sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).output.classesDir;
+        def javaPluginConvention = project.convention.getPlugin(JavaPluginConvention)
+        def classDir = javaPluginConvention.sourceSets.getByName(MAIN_SOURCE_SET_NAME).output.classesDir
 
-        File pluginImpl = new File(classDir, "META-INF/services/hudson.Plugin");
+        File pluginImpl = new File(classDir, 'META-INF/services/hudson.Plugin')
         if (pluginImpl.exists()) {
-            this["Plugin-Class"] = pluginImpl.readLines("UTF-8")[0]
+            this['Plugin-Class'] = pluginImpl.readLines('UTF-8')[0]
         }
 
-        this["Group-Id"] = project.group;
-        this["Short-Name"] = conv.shortName;
-        this["Long-Name"] = conv.displayName;
-        this["Url"] = conv.url;
-        this["Compatible-Since-Version"] = conv.compatibleSinceVersion;
-        if (conv.sandboxStatus)
-            this["Sandbox-Status"] = conv.sandboxStatus;
+        this['Group-Id'] = project.group
+        this['Short-Name'] = conv.shortName
+        this['Long-Name'] = conv.displayName
+        this['Url'] = conv.url
+        this['Compatible-Since-Version'] = conv.compatibleSinceVersion
+        if (conv.sandboxStatus) {
+            this['Sandbox-Status'] = conv.sandboxStatus
+        }
 
         v = project.version
-        if (v==Project.DEFAULT_VERSION)     v = "1.0-SNAPSHOT";
-        if(v.toString().endsWith("-SNAPSHOT")) {
-            String dt = new SimpleDateFormat("MM/dd/yyyy HH:mm").format(new Date());
-            v += " (private-"+dt+"-"+System.getProperty("user.name")+")";
+        if (v == Project.DEFAULT_VERSION) {
+            v = '1.0-SNAPSHOT'
         }
-        this["Plugin-Version"] = v;
+        if (v.toString().endsWith('-SNAPSHOT')) {
+            String dt = new SimpleDateFormat('MM/dd/yyyy HH:mm', Locale.default).format(new Date())
+            v += " (private-$dt-${System.getProperty('user.name')})"
+        }
+        this['Plugin-Version'] = v
 
-        this["Jenkins-Version"] = conv.coreVersion;
+        this['Jenkins-Version'] = conv.coreVersion
 
-        this["Mask-Classes"] = conv.maskClasses;
+        this['Mask-Classes'] = conv.maskClasses
 
-        def dep = findDependencyProjects(project);
-        if(dep.length()>0)
-            this["Plugin-Dependencies"] = dep;
+        def dep = findDependencyProjects(project)
+        if (dep.length() > 0) {
+            this['Plugin-Dependencies'] = dep
+        }
 
         if (conv.pluginFirstClassLoader) {
             this['PluginFirstClassLoader'] = true
@@ -74,47 +82,48 @@ class JpiManifest extends HashMap<String,Object> {
         // more TODO
 /*
         if (project.getDevelopers() != null) {
-            mainSection.addAttributeAndCheck(new Attribute("Plugin-Developers",getDevelopersForManifest()));
+            mainSection.addAttributeAndCheck(new Attribute("Plugin-Developers",getDevelopersForManifest()))
         }
 
-        Boolean b = isSupportDynamicLoading();
+        Boolean b = isSupportDynamicLoading()
         if (b!=null)
-            mainSection.addAttributeAndCheck(new Attribute("Support-Dynamic-Loading",b.toString()));
+            mainSection.addAttributeAndCheck(new Attribute("Support-Dynamic-Loading",b.toString()))
 */
         // remove null values
         for (Iterator itr = this.entrySet().iterator(); itr.hasNext();) {
-            if (itr.next().value==null) itr.remove();
-        }
-    }
-    
-    private String findDependencyProjects(Project project) {
-        def buf = new StringBuilder();
-
-        listUpDependencies(project.configurations.getByName(JpiPlugin.PLUGINS_DEPENDENCY_CONFIGURATION_NAME), false, buf)
-        listUpDependencies(project.configurations.getByName(JpiPlugin.OPTIONAL_PLUGINS_DEPENDENCY_CONFIGURATION_NAME), true, buf)
-
-        return buf.toString();
-    }
-
-    private listUpDependencies(Configuration c, boolean optional, StringBuilder buf) {
-        for (ResolvedArtifact a: c.resolvedConfiguration.resolvedArtifacts) {
-            if (buf.length() > 0)
-                buf.append(',');
-            buf.append(a.moduleVersion.id.name);
-            buf.append(':');
-            buf.append(a.moduleVersion.id.version);
-            if (optional) {
-                buf.append(";resolution:=optional");
+            if (itr.next().value == null) {
+                itr.remove()
             }
         }
     }
 
+    private static String findDependencyProjects(Project project) {
+        def buf = new StringBuilder()
 
+        listUpDependencies(project.configurations.getByName(PLUGINS_DEPENDENCY_CONFIGURATION_NAME), false, buf)
+        listUpDependencies(project.configurations.getByName(OPTIONAL_PLUGINS_DEPENDENCY_CONFIGURATION_NAME), true, buf)
 
-    public void writeTo(File f) {
-        def m = new java.util.jar.Manifest()
-        m.mainAttributes.putValue("Manifest-Version","1.0")
-        this.each {k,v -> m.mainAttributes.putValue(k,v.toString())}
-        f.withOutputStream { o -> m.write(o); }
+        buf.toString()
+    }
+
+    private static listUpDependencies(Configuration c, boolean optional, StringBuilder buf) {
+        for (ResolvedArtifact a : c.resolvedConfiguration.resolvedArtifacts) {
+            if (buf.length() > 0) {
+                buf.append(',')
+            }
+            buf.append(a.moduleVersion.id.name)
+            buf.append(':')
+            buf.append(a.moduleVersion.id.version)
+            if (optional) {
+                buf.append('resolution:=optional')
+            }
+        }
+    }
+
+    void writeTo(File f) {
+        def m = new Manifest()
+        m.mainAttributes.putValue('Manifest-Version', '1.0')
+        this.each { k, v -> m.mainAttributes.putValue(k, v.toString()) }
+        f.withOutputStream { o -> m.write(o) }
     }
 }
