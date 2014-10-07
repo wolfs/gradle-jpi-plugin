c687b61da9ab6728def85c8042257bee224b7808
Daniel Spilker <mail@daniel-spilker.com>
2014-10-08 00:12:05 +0200

    added org.jenkins-ci.jpi as alternative qualified plugin id for Gradle plugin portal inclusion

--- src/test/groovy/org/jenkinsci/gradle/plugins/jpi/JpiPluginSpec.groovy
+++ src/test/groovy/org/jenkinsci/gradle/plugins/jpi/JpiPluginSpec.groovy
@@ -5,9 +5,21 @@ import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
 class JpiPluginSpec extends Specification {
+    Project project = ProjectBuilder.builder().build()
+
+    def 'test plugin id'(String pluginId) {
+        when:
+        project.apply plugin: pluginId
+
+        then:
+        project.plugins[pluginId] instanceof JpiPlugin
+
+        where:
+        pluginId << ['jpi', 'org.jenkins-ci.jpi']
+    }
+
     def 'tasks exist'(String taskName, Class taskClass) {
         when:
-        Project project = ProjectBuilder.builder().build()
         project.apply plugin: 'jpi'
 
         then:
