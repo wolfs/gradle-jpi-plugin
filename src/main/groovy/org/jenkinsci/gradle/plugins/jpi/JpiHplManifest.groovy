460f738cac27312e3b6e407a825c8ceb94e1b276
Daniel Spilker <mail@daniel-spilker.com>
2014-10-07 21:02:15 +0200

    fixed CodeNarc violations

--- src/main/groovy/org/jenkinsci/gradle/plugins/jpi/JpiHplManifest.groovy
+++ src/main/groovy/org/jenkinsci/gradle/plugins/jpi/JpiHplManifest.groovy
@@ -13,26 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.jenkinsci.gradle.plugins.jpi
 
-package org.jenkinsci.gradle.plugins.jpi;
-
-import org.gradle.api.Project;
-import org.gradle.api.plugins.WarPluginConvention;
+import org.gradle.api.Project
+import org.gradle.api.plugins.WarPluginConvention
 
 /**
  * @author Kohsuke Kawaguchi
  */
 class JpiHplManifest extends JpiManifest {
     JpiHplManifest(Project project) {
-        super(project);
+        super(project)
 
         def conv = project.extensions.getByType(JpiExtension)
 
         // src/main/webApp
         def warconv = project.convention.getPlugin(WarPluginConvention)
-        this["Resource-Path"] = warconv.webAppDir.absolutePath
+        this['Resource-Path'] = warconv.webAppDir.absolutePath
 
         // add resource directories directly so that we can pick up the source, then add all the jars and class path
-        this["Libraries"] = (conv.mainSourceTree().resources.srcDirs + conv.runtimeClasspath.files).join(",")
+        this['Libraries'] = (conv.mainSourceTree().resources.srcDirs + conv.runtimeClasspath.files).join(',')
     }
 }
