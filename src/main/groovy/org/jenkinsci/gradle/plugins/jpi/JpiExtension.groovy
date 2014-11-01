wolfs/gradle-jpi-plugin

commit f63269d9c2e243888cc9e2e680d94c2c2342b336

replaced usages of deprecated groovy configuration by compile configuration

src/main/groovy/org/jenkinsci/gradle/plugins/jpi/JpiExtension.groovy
@@ -17,9 +17,9 @@ package org.jenkinsci.gradle.plugins.jpi
 
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
+import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.plugins.WarPlugin
-import org.gradle.api.plugins.GroovyBasePlugin
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.GradleException
 import org.gradle.util.ConfigureUtil
@@ -256,7 +256,7 @@ class JpiExtension {
      */
     FileCollection getRuntimeClasspath() {
         def providedRuntime = project.configurations.getByName(WarPlugin.PROVIDED_RUNTIME_CONFIGURATION_NAME)
-        def groovyRuntime = project.configurations.getByName(GroovyBasePlugin.GROOVY_CONFIGURATION_NAME)
+        def groovyRuntime = project.configurations.getByName(JavaPlugin.COMPILE_CONFIGURATION_NAME)
         mainSourceTree().runtimeClasspath - providedRuntime - groovyRuntime
     }
 

commit 01feac33bb105e1d2503c3261a81acd1196749a0

fixed regression introduced in f63269d9c2e243888cc9e2e680d94c2c2342b336, JENKINS-25401

src/main/groovy/org/jenkinsci/gradle/plugins/jpi/JpiExtension.groovy
@@ -17,7 +17,6 @@ package org.jenkinsci.gradle.plugins.jpi
 
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
-import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.plugins.WarPlugin
 import org.gradle.api.tasks.SourceSet
@@ -247,8 +246,7 @@ class JpiExtension {
      */
     FileCollection getRuntimeClasspath() {
         def providedRuntime = project.configurations.getByName(WarPlugin.PROVIDED_RUNTIME_CONFIGURATION_NAME)
-        def groovyRuntime = project.configurations.getByName(JavaPlugin.COMPILE_CONFIGURATION_NAME)
-        mainSourceTree().runtimeClasspath - providedRuntime - groovyRuntime
+        mainSourceTree().runtimeClasspath - providedRuntime
     }
 
     SourceSet mainSourceTree() {