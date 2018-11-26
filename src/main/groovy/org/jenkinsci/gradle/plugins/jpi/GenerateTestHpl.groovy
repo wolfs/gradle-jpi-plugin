5ddbf44ca974b73abf2d0efdab341ea16546ef7c
Steve Hill <sghill.dev@gmail.com>
2018-11-26 12:33:06 -0800

    Use ObjectFactory#directoryProperty() in Gradle 5.+
    DefaultTask#newOutputDirectory is deprecated in Gradle 5.0. This change
    retains backwards compatibility.

--- src/main/groovy/org/jenkinsci/gradle/plugins/jpi/GenerateTestHpl.groovy
+++ src/main/groovy/org/jenkinsci/gradle/plugins/jpi/GenerateTestHpl.groovy
@@ -2,15 +2,22 @@ package org.jenkinsci.gradle.plugins.jpi
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import org.gradle.util.GradleVersion
 
 class GenerateTestHpl extends DefaultTask {
+    private static final GradleVersion GRADLE_5_0 = GradleVersion.version('5.0')
     @OutputDirectory
     DirectoryProperty hplDir
 
     GenerateTestHpl() {
-        this.hplDir = newOutputDirectory()
+        if (GradleVersion.current() >= GRADLE_5_0) {
+            this.hplDir = services.get(ObjectFactory).directoryProperty()
+        } else {
+            this.hplDir = newOutputDirectory()
+        }
     }
 
     @TaskAction
