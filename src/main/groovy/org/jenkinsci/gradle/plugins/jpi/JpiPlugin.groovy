wolfs/gradle-jpi-plugin

commit 7a565d3d51b400e67ce5643e3f71b9113c194296

Lazily register generate-test-hpl task

src/main/groovy/org/jenkinsci/gradle/plugins/jpi/JpiPlugin.groovy
@@ -400,9 +400,11 @@ class JpiPlugin implements Plugin<Project> {
         SourceSet testSourceSet = javaConvention.sourceSets.getByName(TEST_SOURCE_SET_NAME)
 
         // generate test hpl manifest for the current plugin, to be used during unit test
-        GenerateTestHpl generateTestHpl = project.tasks.create('generate-test-hpl', GenerateTestHpl)
-        generateTestHpl.hplDir.set(project.layout.buildDirectory.dir('generated-resources/test'))
-        testSourceSet.output.dir(generateTestHpl.hplDir)
+        def outputDir = project.layout.buildDirectory.dir('generated-resources/test')
+        project.tasks.register('generate-test-hpl', GenerateTestHpl) {
+            it.hplDir.set(outputDir)
+        }
+        testSourceSet.output.dir(outputDir)
     }
 
     private static void resolvePluginDependencies(Project project) {

commit 7854ee06b57a658b6b06611e97049dae4b89e4ac

Fix generate-test-hpl task dependency for running tests
This issue was introduced in commit 7a565d3d51b400e67ce5643e3f71b9113c194296,
when lazily registering the task.

src/main/groovy/org/jenkinsci/gradle/plugins/jpi/JpiPlugin.groovy
@@ -422,10 +422,13 @@ class JpiPlugin implements Plugin<Project> {
 
         // generate test hpl manifest for the current plugin, to be used during unit test
         def outputDir = project.layout.buildDirectory.dir('generated-resources/test')
-        project.tasks.register('generate-test-hpl', GenerateTestHpl) {
+        testSourceSet.output.dir(outputDir)
+
+        def generateTestHplTask = project.tasks.register('generate-test-hpl', GenerateTestHpl) {
             it.hplDir.set(outputDir)
         }
-        testSourceSet.output.dir(outputDir)
+
+        project.tasks.named(JavaPlugin.TEST_CLASSES_TASK_NAME).configure { it.dependsOn(generateTestHplTask) }
     }
 
     private static void resolvePluginDependencies(Project project) {

src/test/groovy/org/jenkinsci/gradle/plugins/jpi/JpiIntegrationSpec.groovy
@@ -166,6 +166,7 @@ class JpiIntegrationSpec extends IntegrationSpec {
         'processTestResources' | ':resolveTestDependencies' | TaskOutcome.NO_SOURCE
         'jpi'                  | ':war'                     | TaskOutcome.SUCCESS
         'compileTestJava'      | ':insertTest'              | TaskOutcome.SKIPPED
+        'testClasses'          | ':generate-test-hpl'       | TaskOutcome.SUCCESS
         'compileJava'          | ':localizer'               | TaskOutcome.SUCCESS
     }
 